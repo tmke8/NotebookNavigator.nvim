9e6a59679e9025a0e714d7089410e1c0d986d15d
Thomas MK <tmke8@posteo.net>
2026-05-20 23:15:17 +0100

    Remove nvim-treesitter dependency

--- lua/notebook-navigator/core.lua
+++ lua/notebook-navigator/core.lua
@@ -1,6 +1,7 @@
 local commenter = require "notebook-navigator.commenters"
 local get_repl = require "notebook-navigator.repls"
-local ts_queries = require('nvim-treesitter.query')
+local ts = vim.treesitter
+local ts_range = ts._range or require('nvim-treesitter-textobjects._range')
 
 local M = {}
 
@@ -30,7 +31,8 @@ M.miniai_spec = function(opts, cell_marker)
 end
 
 M.map_to_range = function(m)
-  local range = vim.treesitter.get_range(m.node, buf_id, m.metadata)
+  -- local range = vim.treesitter.get_range(m.node, buf_id, m.metadata)
+  local range = m
 
   -- map to 1-indexed lines and 0-indexed columns
   local from = { line = range[1] + 1, col = range[2] }
@@ -39,9 +41,167 @@ M.map_to_range = function(m)
   return { from = from, to = to }
 end
 
+---@param object table
+---@param path string[]
+---@param value table
+local function insert_to_path(object, path, value)
+  ---@type table<string, any|table<string, any>>
+  local curr_obj = object
+
+  for index = 1, (#path - 1) do
+    if curr_obj[path[index]] == nil then
+      curr_obj[path[index]] = {}
+    end
+
+    ---@type table<string, any|table<string, any>>
+    curr_obj = curr_obj[path[index]]
+  end
+
+  ---@type table<string, any|table<string, any>>
+  curr_obj[path[#path]] = value
+end
+
+---Memoize a function using hash_fn to hash the arguments.
+---@generic F: function
+---@param fn F
+---@param hash_fn fun(...): any
+---@return F
+local function memoize(fn, hash_fn)
+  local cache = setmetatable({}, { __mode = 'kv' }) ---@type table<any,any>
+
+  return function(...)
+    local key = hash_fn(...)
+    if cache[key] == nil then
+      local v = fn(...) ---@type any
+      cache[key] = v ~= nil and v or vim.NIL
+    end
+
+    local v = cache[key]
+    return v ~= vim.NIL and v or nil
+  end
+end
+
+--- Prepare matches for given query_group and parsed tree
+--- memoize by buffer tick and query group
+---
+---@param bufnr integer the buffer
+---@param query_group string the query file to use
+---@param root TSNode the root node
+---@param root_lang string the root node lang, if known
+---@return table[]
+local get_query_matches = memoize(function(bufnr, query_group, root, root_lang)
+  local query = ts.query.get(root_lang, query_group)
+  if not query then
+    return {}
+  end
+
+  local matches = {} ---@type table[]
+  local start_row, _, end_row, _ = root:range()
+  -- The end row is exclusive so we need to add 1 to it.
+  for pattern, match, metadata in query:iter_matches(root, bufnr, start_row, end_row + 1) do
+    if pattern then
+      local prepared_match = {}
+
+      -- Extract capture names from each match
+      for id, nodes in pairs(match) do
+        local query_name = query.captures[id] -- name of the capture in the query
+        if query_name ~= nil then
+          local path = vim.split(query_name, '%.')
+          if metadata[id] and metadata[id].range then
+            insert_to_path(prepared_match, path, ts_range.add_bytes(bufnr, metadata[id].range))
+          else
+            local srow, scol, sbyte, erow, ecol, ebyte = nodes[1]:range(true)
+            if #nodes > 1 then
+              local _, _, _, e_erow, e_ecol, e_ebyte = nodes[#nodes]:range(true)
+              erow = e_erow
+              ecol = e_ecol
+              ebyte = e_ebyte
+            end
+            insert_to_path(prepared_match, path, { srow, scol, sbyte, erow, ecol, ebyte })
+          end
+        end
+      end
+
+      if metadata.range and metadata.range[7] then
+        ---@cast metadata TSTextObjects.Metadata
+        local query_name = metadata.range[7]
+        local path = vim.split(query_name, '%.')
+        insert_to_path(prepared_match, path, {
+          metadata.range[1],
+          metadata.range[2],
+          metadata.range[3],
+          metadata.range[4],
+          metadata.range[5],
+          metadata.range[6],
+        })
+      end
+
+      matches[#matches + 1] = prepared_match
+    end
+  end
+  return matches
+end, function(bufnr, query_group, root)
+  return string.format('%d-%s-%s', bufnr, root:id(), query_group)
+end)
+
+---@param tbl table<string, any|table<string, any>> the table to access
+---@param path string the '.' separated path
+---@return any|nil result the value at path or nil
+local function get_at_path(tbl, path)
+  if path == '' then
+    return tbl
+  end
+
+  local segments = vim.split(path, '%.')
+  local result = tbl
+
+  for _, segment in ipairs(segments) do
+    if type(result) == 'table' then
+      ---@type any
+      result = result[segment]
+    end
+  end
+
+  return result
+end
+
+---@param bufnr integer
+---@param query_string string
+---@param query_group string
+---@return Range6[]
+local function get_capture_ranges_recursively(bufnr, query_string, query_group)
+  if query_string:sub(1, 1) ~= '@' then
+    error('Captures must start with "@"')
+    return {}
+  end
+  query_string = query_string:sub(2)
+
+  local parser = ts.get_parser(bufnr)
+  if not parser then
+    return {}
+  end
+  parser:parse(true)
+
+  local ranges = {} ---@type Range6[]
+  parser:for_each_tree(function(tree, lang_tree)
+    local tree_lang = lang_tree:lang()
+
+    local matches = get_query_matches(bufnr, query_group, tree:root(), tree_lang)
+    for _, match in pairs(matches) do
+      local found = get_at_path(match, query_string)
+      if found then
+        ---@cast found Range6
+        table.insert(ranges, found)
+      end
+    end
+  end)
+
+  return ranges
+end
+
 M.get_toplevels = function()
   local buf_id = vim.api.nvim_get_current_buf()
-  local matches = ts_queries.get_capture_matches_recursively(buf_id, '@toplevel', 'toplevels')
+  local matches = get_capture_ranges_recursively(buf_id, '@toplevel', 'toplevels')
   return vim.tbl_map(M.map_to_range, matches)
 end
 
